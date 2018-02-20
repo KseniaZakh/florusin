c32db8e1dc86937311f77dcd53d962cc9bd1eb48
KseniaZakh <cassie3@yandex.ru>
2018-02-20 08:43:43 +0500

    web-form correction

---
+++ js/card.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	$("ul.gallery > li").click(function() {
+		$("ul.gallery > li").removeClass("selected");
+			$(this).addClass("selected");
+			var image = $(this).html();
+			$("div.card_photo > div:first-child").html(image);
+		});
+
+		$("ul.card_choiсe > li").click(function() {
+			$("ul.card_choiсe > li").removeClass("selected");
+			$(this).addClass("selected");
+				if ($("ul.card_choiсe > li:first-child").hasClass("selected")) {
+				$("div.price").hide();
+				$("div.description").show();
+			}
+			if ($("ul.card_choiсe > li:last-child").hasClass("selected")) {
+				$("div.description").hide();
+				$("div.price").show();
+			}
+		});
+});
