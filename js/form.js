9ecb3ad01ec20de94b8f8cabaaddb2bdbf99a0a1
KseniaZakh <cassie3@yandex.ru>
2018-02-22 20:49:02 +0500

    form correction-3

--- js/form.js
+++ js/form.js
@@ -4,16 +4,33 @@ $(document).ready(function() {
 		var element = this;
 		setTimeout(function () {
 			if (!$(element).val()) {
-				if ($(element).next("span").hasClass("selected")) {
-					$(element).next("span").removeClass("selected");
+				if ($(element).prev("span").hasClass("selected")) {
+					$(element).prev("span").removeClass("selected");
 					}
 				}
 			else {
-				if (!$(element).next("span").hasClass("selected")) {
-					$(element).next("span").addClass("selected");
+				if (!$(element).prev("span").hasClass("selected")) {
+					$(element).prev("span").addClass("selected");
 					}
 				}
 			}, 100);
 		});
+
+		$("div.message > textarea").on("propertychange change click keyup input paste", function() {
+			var element = this;
+			setTimeout(function () {
+				if (!$(element).val()) {
+					if ($(element).prev("span").hasClass("selected")) {
+						$(element).prev("span").removeClass("selected");
+						}
+					}
+				else {
+					if (!$(element).prev("span").hasClass("selected")) {
+						$(element).prev("span").addClass("selected");
+						}
+					}
+				}, 100);
+			});
+
 
 	});
