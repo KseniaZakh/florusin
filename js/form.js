cd025180019a435be2494992f1364e3437bc5685
KseniaZakh <cassie3@yandex.ru>
2018-02-24 17:36:39 +0500

    form.js checkbox

--- js/form.js
+++ js/form.js
@@ -1,36 +1,94 @@
 $(document).ready(function() {
 
-	$("input[type=text]").on("propertychange change click keyup input paste", function() {
+	$("input[type=text]").on("blur", function() {
+		if (!$(this).val()) {
+			if ($(this).prev("span").hasClass("selected")) {
+				$(this).prev("span").removeClass("selected");
+				}
+			}
+		else {
+			if (!$(this).prev("span").hasClass("selected")) {
+				$(this).prev("span").addClass("selected");
+				}
+			}
+		});
+
+		function isName(name) {
+			var regex = new RegExp(/^([а-яА-Яa-zA-Z _.-]{3,30})+$/);
+  			return regex.test(name);
+		}
+
+		function isEmail(email) {
+			var regex = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+  			return regex.test(email);
+		}
+
+		$("input[type=text]").on("propertychange change click keyup input paste", function() {
 		var element = this;
+		if (!$(element).prev("span").hasClass("selected")) {
+			$(element).prev("span").addClass("selected");
+			}
 		setTimeout(function () {
 			if (!$(element).val()) {
-				if ($(element).prev("span").hasClass("selected")) {
-					$(element).prev("span").removeClass("selected");
+				if ($(element).hasClass("error")) {
+					$(element).removeClass("error");
+					}
+				if ($(element).hasClass("success")) {
+					$(element).removeClass("success");
 					}
 				}
 			else {
-				if (!$(element).prev("span").hasClass("selected")) {
-					$(element).prev("span").addClass("selected");
+				if ($(element).parent("li").hasClass("required")) {
+					if ($(element).attr("name") == "username") {
+						var result = isName($(element).val());
+						}
+					if ($(element).attr("name") == "user_email") {
+						var result = isEmail($(element).val());
+						}
+					if (!result) {
+						if (!$(element).hasClass("error")) {
+							$(element).removeClass("success").addClass("error");
+							}
+						}
+					else {
+						if (!$(element).hasClass("success")) {
+							$(element).removeClass("error").addClass("success");
+							}
+						}
 					}
 				}
 			}, 100);
 		});
 
-		$("div.message > textarea").on("propertychange change click keyup input paste", function() {
-			var element = this;
-			setTimeout(function () {
-				if (!$(element).val()) {
-					if ($(element).prev("span").hasClass("selected")) {
-						$(element).prev("span").removeClass("selected");
-						}
-					}
-				else {
-					if (!$(element).prev("span").hasClass("selected")) {
-						$(element).prev("span").addClass("selected");
-						}
-					}
-				}, 100);
-			});
+		// $("div.message > textarea").on("propertychange change click keyup input paste", function() {
+		// 	var element = this;
+		// 	if (!$(element).prev("span").hasClass("selected")) {
+		// 		$(element).prev("span").addClass("selected");
+		// 		}
+		// 	});
+		//
+		// 	$("div.message > textarea").on("blur", function() {
+		// 		if (!$(this).val()) {
+		// 			if ($(this).prev("span").hasClass("selected")) {
+		// 				$(this).prev("span").removeClass("selected");
+		// 			}
+		// 		}
+		// 		else {
+		// 			if (!$(this).prev("span").hasClass("selected")) {
+		// 				$(this).prev("span")addClass("selected");
+		// 			}
+		// 		}
+		// 	});
+
+		$("span.checkbox").on ("click",function() {
+			var elem = this;
+			if (!$(elem).hasClass("checked")) {
+				$(elem).addClass("checked");
+			}
+			else {
+				$(elem).removeClass("checked");
+			}
+		});
 
 
-	});
+		});
